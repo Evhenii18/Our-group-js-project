1c64c888cd6d2272139560170ee051bb08e29512
Yevhenii Baranik <baranik18@gmail.com>
2024-10-10 20:08:46 +0100

    libraries added

---
+++ src/main.js
@@ -0,0 +1,6 @@
+
+import iziToast from "izitoast";
+import 'izitoast/dist/css/iziToast.min.css';
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+import 'normalize.css';
