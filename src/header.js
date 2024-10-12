cbe15f4016074b88b1d994e8a89dd660a523dd75
Oleh18zb <110353014+Oleh18zb@users.noreply.github.com>
2024-10-12 19:48:57 +0200

    подобавляв

---
+++ src/header.js
@@ -0,0 +1,13 @@
+const modalBtn = document.querySelector('.menu-icon-btn');
+const closeModal = document.querySelector('.header-modal-btn');
+
+modalBtn.addEventListener('click', toggleMenu);
+closeModal.addEventListener('click', croseClose);
+
+function toggleMenu(event) {
+    
+}
+
+function croseClose(event) {
+    
+}
