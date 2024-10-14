07b26951f0f7aea186760bf01fb2880b4b54bd09
EevgenK <45nalitay45@gmail.com>
2024-10-14 22:17:42 +0300

    check2

---
+++ src/js/switch-btn/themeColorToggle.js
@@ -0,0 +1,21 @@
+const toggleButton = document.getElementById('theme-toggle');
+const body = document.body;
+
+const addClassList = (element, darkClass) =>
+  localStorage.getItem('theme') === 'dark' && element.classList.add(darkClass);
+
+const themeColorToggle = () => {
+  toggleButton.classList.toggle('active-btn');
+  body.classList.toggle('dark-theme');
+
+  body.classList.contains('dark-theme')
+    ? localStorage.setItem('theme', 'dark')
+    : localStorage.setItem('theme', 'light');
+};
+
+addClassList(body, 'dark-theme');
+
+toggleButton.addEventListener('click', themeColorToggle);
+export default themeColorToggle;
+
+/* check */
